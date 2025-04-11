e4974873dcf6808ea79c9e0a49f97ad428e86e8c
martinolanza03 <lanzamartino3@gmail.com>
2025-04-11 15:35:10 +0200

    feat: create actor card

--- src/App.jsx
+++ src/App.jsx
@@ -10,13 +10,23 @@ function App() {
 
   function fetchActors() {
     axios.get(endPoint)
-      .then(res => console.log(res.data))
+      .then(res => setActors(res.data));
   }
 
-  useEffect(fetchActors, [])
+  useEffect(fetchActors, []);
 
   return (
-    <></>
+    <ul>
+      {actors.map((actor, i) => (
+        <li key={i}>
+          <h2 >{actor.name}</h2>
+          <p>Birthday: {actor.birth_year}</p>
+          <p>Nationality: {actor.nationality}</p>
+          <p>Biography: {actor.biography}</p>
+          <img src={actor.image} alt={actor.name} />
+          <p>Known for: {actor.known_for}</p>
+        </li>))}
+    </ul>
   )
 }
 
